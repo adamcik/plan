d45d525c6a01b93dd13bfd073d3541795ff73a59
Thomas Adamcik <timeplan@adamcik.no>
2008-07-24 16:47:55 +0200

    Added getting_started page to url and scrape_list debug

--- plan/common/urls.py
+++ plan/common/urls.py
@@ -3,10 +3,11 @@ from django.conf.urls.defaults import *
 from plan.common.models import Room, Course
 
 urlpatterns = patterns('plan.common.views',
-    url(r'^$', 'schedule'),
+    url(r'^$', 'getting_started'),
     url(r'^s/(?P<slug>[a-zA-Z0-9-_]+)/$', 'schedule', name='schedule'),
     url(r'^s/(?P<slug>[a-zA-Z0-9-_]+)/(?P<year>\d{4})/(?P<semester>\w+)/$', 'schedule'),
 
+    url(r'^debug/$', 'scrape_list'),
     url(r'^debug/(?P<course>[\w\d-]+)/$', 'scrape'),
     url(r'^s/(?P<slug>[a-zA-Z0-9-_]+)/alter/$', 'select_course', name='select_course'),
     url(r'^s/(?P<slug>[a-zA-Z0-9-_]+)/groups/$', 'select_groups', name='select_groups'),
