56c4c301082bd13a37fa61682d1fea6b313bdab0
Thomas Adamcik <timeplan@adamcik.no>
2008-07-24 16:44:13 +0200

    Updated settings file

--- plan/settings.py
+++ plan/settings.py
@@ -1,6 +1,9 @@
 # Django settings for plan project.
 
-DEBUG = True
+import os
+BASE_PATH = os.path.dirname(os.path.abspath(__file__)) + '/..'
+
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 LOGGING_OUTPUT_ENABLED = DEBUG
 LOGGING_LOG_SQL = DEBUG
@@ -10,13 +13,6 @@ ADMINS = (
 )
 
 MANAGERS = ADMINS
-
-DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = '../plan.sqlite'             # Or path to database file if using sqlite3.
-DATABASE_USER = ''             # Not used with sqlite3.
-DATABASE_PASSWORD = ''         # Not used with sqlite3.
-DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -37,7 +33,7 @@ USE_I18N = False
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = '/home/adamcik/dev/plan/media'
+MEDIA_ROOT = BASE_PATH + '/media'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
@@ -48,9 +44,6 @@ MEDIA_URL = '/media/'
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
 ADMIN_MEDIA_PREFIX = '/media/admin'
-
-# Make this unique, and don't share it with anybody.
-SECRET_KEY = 'noq(=&z#@ga9dxsqrr3wg#f^&njh)pp^@ado5fh)dqpb-3!15r'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
@@ -73,7 +66,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/home/adamcik/dev/plan/plan/templates',
+    BASE_PATH + '/plan/templates',
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
@@ -92,4 +85,7 @@ INSTALLED_APPS = (
     'plan.common',
 )
 
-INTERNAL_IPS = ['127.0.0.1']
+try:
+    from settings_local import *
+except ImportError:
+    pass
