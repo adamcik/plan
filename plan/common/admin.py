4fef8e2faee2125a082e844ea6d10613e5cb0d45
Thomas Adamcik <timeplan@adamcik.no>
2008-07-24 16:46:27 +0200

    Admin site fixing

--- plan/common/admin.py
+++ plan/common/admin.py
@@ -1,11 +1,24 @@
 from django.contrib import admin
 from plan.common.models import *
 
-admin.site.register(UserSet)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('name', 'full_name')
+    search_fields = ('name', 'full_name')
+
+class UserSetAdmin(admin.ModelAdmin):
+    list_display = ('slug', 'course')
+    search_fields = ('slug', 'course')
+    list_filter = ['slug']
+
+class LectureAdmin(admin.ModelAdmin):
+    list_display = ('course', 'day', 'start_time', 'end_time', 'room')
+    search_fields = list_display
+    list_filter = ['day', 'start_time', 'room']
+
+admin.site.register(UserSet, UserSetAdmin)
 admin.site.register(Type)
 admin.site.register(Room)
 admin.site.register(Group)
-admin.site.register(Week)
-admin.site.register(Course)
-admin.site.register(Lecture)
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Lecture, LectureAdmin)
 admin.site.register(Semester)
