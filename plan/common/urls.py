edc765dc6294ec2093791301b1f9437394ba642a
Thomas Adamcik <timeplan@adamcik.no>
2008-07-25 14:02:40 +0200

    Added urlname

--- plan/common/urls.py
+++ plan/common/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.defaults import *
 from plan.common.models import Room, Course
 
 urlpatterns = patterns('plan.common.views',
-    url(r'^$', 'getting_started'),
+    url(r'^$', 'getting_started', name="frontpage"),
     url(r'^s/(?P<slug>[a-zA-Z0-9-_]+)/$', 'schedule', name='schedule'),
     url(r'^s/(?P<slug>[a-zA-Z0-9-_]+)/(?P<year>\d{4})/(?P<semester>\w+)/$', 'schedule'),
 
