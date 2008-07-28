512630a7b3779927be58b37d3d369587ba397215
Thomas Adamcik <timeplan@adamcik.no>
2008-07-28 20:15:49 +0200

    Added internal ip middlware to default settings

--- plan/settings.py
+++ plan/settings.py
@@ -56,6 +56,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'plan.common.middleware.InternalIpMiddleware',
     'django.middleware.doc.XViewMiddleware',
     'djangologging.middleware.LoggingMiddleware',
 )
