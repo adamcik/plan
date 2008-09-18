fefcc935c607dd0a5319e3aa2ee749dbc29191d5
Thomas Adamcik <timeplan@adamcik.no>
2008-09-18 14:01:19 +0200

    Update studweb xml scraper to handle multiple xml files at once

--- plan/scrape/studweb.py
+++ plan/scrape/studweb.py
@@ -2,79 +2,93 @@ from urllib import urlopen
 from xml.dom import minidom
 from dateutil.parser import parse
 
+from django.db import transaction
+
 from plan.common.models import *
 
-def import_xml(url):
-    dom = minidom.parseString(urlopen(url).read())
+@transaction.commit_on_success
+def import_xml(urls):
+    added, updated = [], []
 
-    ids = []
+    if type(urls) not in (list, tuple):
+        urls = [urls]
 
-    for n in dom.getElementsByTagName('dato_row'):
-        exam_kwargs = {}
+    for url in urls:
+        dom = minidom.parseString(urlopen(url).read())
 
-        course_code = n.getElementsByTagName('emnekode')[0].firstChild
+        for n in dom.getElementsByTagName('dato_row'):
+            exam_kwargs = {}
 
-        exam_date = n.getElementsByTagName('dato_eksamen')[0].firstChild
-        exam_time = n.getElementsByTagName('klokkeslett_fremmote_tid')[0].firstChild
+            course_code = n.getElementsByTagName('emnekode')[0].firstChild
 
-        handout_date = n.getElementsByTagName('dato_uttak')[0].firstChild
-        handout_time = n.getElementsByTagName('klokkeslett_uttak')[0].firstChild
+            exam_date = n.getElementsByTagName('dato_eksamen')[0].firstChild
+            exam_time = n.getElementsByTagName('klokkeslett_fremmote_tid')[0].firstChild
 
-        handin_date = n.getElementsByTagName('dato_innlevering')[0].firstChild
-        handin_time =n.getElementsByTagName('klokkeslett_innlevering')[0].firstChild
+            handout_date = n.getElementsByTagName('dato_uttak')[0].firstChild
+            handout_time = n.getElementsByTagName('klokkeslett_uttak')[0].firstChild
 
-        duration = n.getElementsByTagName('varighettimer')[0].firstChild
+            handin_date = n.getElementsByTagName('dato_innlevering')[0].firstChild
+            handin_time =n.getElementsByTagName('klokkeslett_innlevering')[0].firstChild
 
-        long_typename = n.getElementsByTagName('vurderingskombinasjon_vurdkombnavn_bokmal')[0].firstChild
-        typename = n.getElementsByTagName('vurderingsformkode')[0].firstChild
+            duration = n.getElementsByTagName('varighettimer')[0].firstChild
 
-        status_code = n.getElementsByTagName('vurdstatuskode')[0].firstChild
+            long_typename = n.getElementsByTagName('vurderingskombinasjon_vurdkombnavn_bokmal')[0].firstChild
+            typename = n.getElementsByTagName('vurderingsformkode')[0].firstChild
 
-        comment = n.getElementsByTagName('kommentar_eksamen')[0].firstChild
+            status_code = n.getElementsByTagName('vurdstatuskode')[0].firstChild
 
-        if not status_code or status_code.nodeValue != 'ORD':
-            continue
+            comment = n.getElementsByTagName('kommentar_eksamen')[0].firstChild
 
-        print "Adding exam for %s" % course_code.nodeValue
+            if not status_code or status_code.nodeValue != 'ORD':
+                continue
 
-        course, created = Course.objects.get_or_create(name=course_code.nodeValue)
-        exam_kwargs['course'] = course
+            course, created = Course.objects.get_or_create(name=course_code.nodeValue)
+            exam_kwargs['course'] = course
 
-        if exam_date:
-            exam_kwargs['exam_date'] = parse(exam_date.nodeValue).date()
-        if exam_time:
-            exam_kwargs['exam_time'] = parse(exam_time.nodeValue).time()
+            if exam_date:
+                exam_kwargs['exam_date'] = parse(exam_date.nodeValue).date()
+            if exam_time:
+                exam_kwargs['exam_time'] = parse(exam_time.nodeValue).time()
 
-        if handout_date:
-            exam_kwargs['handout_date'] = parse(handout_date.nodeValue).date()
-        if handout_time:
-            exam_kwargs['handout_time'] = parse(handout_time.nodeValue).time()
+            if handout_date:
+                exam_kwargs['handout_date'] = parse(handout_date.nodeValue).date()
+            if handout_time:
+                exam_kwargs['handout_time'] = parse(handout_time.nodeValue).time()
 
-        if handin_date:
-            exam_kwargs['exam_date'] = parse(handin_date.nodeValue).date()
-        if handin_time:
-            exam_kwargs['exam_time'] = parse(handin_time.nodeValue).time()
+            if handin_date:
+                exam_kwargs['exam_date'] = parse(handin_date.nodeValue).date()
+            if handin_time:
+                exam_kwargs['exam_time'] = parse(handin_time.nodeValue).time()
 
-        if typename:
-            exam_kwargs['type'] = typename.nodeValue
-        else:
-            exam_kwargs['type'] = ''
+            if typename:
+                exam_kwargs['type'] = typename.nodeValue
+            else:
+                exam_kwargs['type'] = ''
 
-        try:
-            exam, created = Exam.objects.get_or_create(**exam_kwargs)
-        except Exam.MultipleObjectsReturned, e:
-            print Exam.objects.filter(**exam_kwargs).values()
-            raise e
+            try:
+                exam, created = Exam.objects.get_or_create(**exam_kwargs)
+            except Exam.MultipleObjectsReturned, e:
+                print Exam.objects.filter(**exam_kwargs).values()
+                raise e
 
-        if duration:
-            exam.duration = duration.nodeValue
+            if created:
+                print "Added exam for %s" % course.name
+                added.append(exam.id)
+            else:
+                print "Updated exam for %s" % course.name
+                updated.append(exam.id)
 
-        if comment:
-            exam.comment = comment.nodeValue
+            if duration:
+                exam.duration = duration.nodeValue
 
-        exam.save()
+            if comment:
+                exam.comment = comment.nodeValue
 
-        ids.append(exam.id)
+            exam.save()
 
-    print Exam.objects.exclude(id__in=ids).values_list('name')
-    Exam.objects.exclude(id__in=ids).delete()
+            n.unlink()
+
+    print 'Added %d exams' % len(added)
+    print 'Updated %d exams' % len(updated)
+    print 'Deleting %d exams' % Exam.objects.exclude(id__in=added+updated).count()
+    Exam.objects.exclude(id__in=added+updated).delete()
