b9ad7ee6078569d6b6f348fa2dc802ca98937957
Thomas Adamcik <timeplan@adamcik.no>
2008-11-02 04:03:22 +0100

    Fix instance label in deadline form

--- plan/common/forms.py
+++ plan/common/forms.py
@@ -19,6 +19,7 @@ class GroupForm(forms.Form):
 
 class DeadlineForm(forms.models.ModelForm):
     '''Form for adding deadlines'''
+
     class Meta:
         model = Deadline
 
@@ -27,6 +28,8 @@ class DeadlineForm(forms.models.ModelForm):
 
         self.fields['userset'].queryset = queryset
         self.fields['userset'].widget.attrs['style'] = 'width: 7em'
+        self.fields['userset'].label_from_instance = lambda obj: obj.course.name
+
         self.fields['time'].widget.attrs['size'] = 2
         self.fields['date'].widget.attrs['size'] = 7
         self.fields['task'].widget.attrs['size'] = 28
