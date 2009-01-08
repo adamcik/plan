5ee2dd8875c000728b378cd5ce59fec889277824
Thomas Adamcik <timeplan@adamcik.no>
2009-01-08 16:29:24 +0100

    Update timtable test

--- plan/common/tests.py
+++ plan/common/tests.py
@@ -60,41 +60,45 @@ class TimetableTestCase(BaseTestCase):
 
         rows = []
 
-        lectures = dict([(l.id, l) for l in lectures])
-        lecture2 = {'lecture': lectures[2], 'rowspan': 2, 'remove': False}
-        lecture3 = {'lecture': lectures[3], 'rowspan': 2, 'remove': False}
-        lecture4 = {'lecture': lectures[4], 'rowspan': 6, 'remove': False}
-        lecture5 = {'lecture': lectures[5], 'rowspan': 2, 'remove': False}
-        lecture8 = {'lecture': lectures[8], 'rowspan': 1, 'remove': False}
+        lectures  = dict([(l.id, l) for l in lectures])
+        lecture2  = {'lecture': lectures[2],  'rowspan': 2,  'remove': False}
+        lecture3  = {'lecture': lectures[3],  'rowspan': 2,  'remove': False}
+        lecture4  = {'lecture': lectures[4],  'rowspan': 6,  'remove': False}
+        lecture5  = {'lecture': lectures[5],  'rowspan': 2,  'remove': False}
+        lecture8  = {'lecture': lectures[8],  'rowspan': 1,  'remove': False}
+        lecture9  = {'lecture': lectures[9],  'rowspan': 12, 'remove': False}
+        lecture10 = {'lecture': lectures[10], 'rowspan': 1,  'remove': False}
+        lecture11 = {'lecture': lectures[11], 'rowspan': 1,  'remove': False}
 
-        rows.append([[lecture2, lecture4, {}],       [{}], [{}], [{}], [{}]])
+        rows.append([[lecture2, lecture4, {}],       [lecture9], [lecture10], [{}], [{}]])
 
         lecture2 = copy(lecture2)
         lecture2['remove'] = True
+        lecture9 = copy(lecture9)
+        lecture9['remove'] = True
 
         lecture4 = copy(lecture4)
         lecture4['remove'] = True
 
-        rows.append([[lecture2, lecture4, lecture5], [{}], [{}], [{}], [{}]])
+        rows.append([[lecture2, lecture4, lecture5], [lecture9], [{}], [{}], [{}]])
 
         lecture5 = copy(lecture5)
         lecture5['remove'] = True
 
-        rows.append([[lecture3, lecture4, lecture5], [{}], [{}], [{}], [{}]])
+        rows.append([[lecture3, lecture4, lecture5], [lecture9], [{}], [{}], [{}]])
 
         lecture3 = copy(lecture3)
         lecture3['remove'] = True
 
-        rows.append([[lecture3, lecture4, {}],       [{}], [{}], [{}], [{}]])
-        rows.append([[{},       lecture4, {}],       [{}], [{}], [{}], [{}]])
-        rows.append([[{},       lecture4, {}],       [{}], [{}], [{}], [{}]])
-        rows.append([[{},       {},       {}],       [{}], [{}], [{}], [{}]])
-        rows.append([[lecture8, {},       {}],       [{}], [{}], [{}], [{}]])
-        rows.append([[{},       {},       {}],       [{}], [{}], [{}], [{}]])
-        rows.append([[{},       {},       {}],       [{}], [{}], [{}], [{}]])
-        rows.append([[{},       {},       {}],       [{}], [{}], [{}], [{}]])
-        rows.append([[{},       {},       {}],       [{}], [{}], [{}], [{}]])
-
+        rows.append([[lecture3, lecture4, {}],       [lecture9], [{}], [{}], [{}]])
+        rows.append([[{},       lecture4, {}],       [lecture9], [{}], [{}], [{}]])
+        rows.append([[{},       lecture4, {}],       [lecture9], [{}], [{}], [{}]])
+        rows.append([[{},       {},       {}],       [lecture9], [{}], [{}], [{}]])
+        rows.append([[lecture8, {},       {}],       [lecture9], [{}], [{}], [{}]])
+        rows.append([[{},       {},       {}],       [lecture9], [{}], [{}], [{}]])
+        rows.append([[{},       {},       {}],       [lecture9], [{}], [{}], [{}]])
+        rows.append([[{},       {},       {}],       [lecture9], [{}], [{}], [{}]])
+        rows.append([[{},       {},       {}],       [lecture9], [lecture11], [{}], [{}]])
 
         for t,r in zip(timetable.table, rows):
             self.assertEquals(t,r)
