5e2fc99ee1b8431da5a3ffa1c8c5ddf7e35ddf5b
Thomas Adamcik <timeplan@adamcik.no>
2009-02-02 15:36:20 +0100

    Refactor cache wrapper

--- plan/common/cache.py
+++ plan/common/cache.py
@@ -1,11 +1,10 @@
 import logging
 from uuid import uuid4
 
-from django.http import QueryDict
 from django.conf import settings
 from django.utils.http import int_to_base36
 from django.core.cache import get_cache
-from django.core.cache.backends.base import InvalidCacheBackendError, BaseCache
+from django.core.cache.backends.base import  BaseCache
 
 logger = logging.getLogger()
 
@@ -24,43 +23,12 @@ def clear_cache(semester, slug):
 
 class CacheClass(BaseCache):
     def __init__(self, *args, **kwargs):
-        backend_uri = settings.CACHE_BACKEND
-
-        if backend_uri.find(':') == -1:
-            raise InvalidCacheBackendError, "Backend URI must start with scheme://"
-        scheme, rest = backend_uri.split(':', 1)
-        if not rest.startswith('//'):
-            raise InvalidCacheBackendError, "Backend URI must start with scheme://"
-
-        host = rest[2:]
-        qpos = rest.find('?')
-        if qpos != -1:
-            params = QueryDict(rest[qpos+1:], mutable=True)
-            host = rest[2:qpos]
-        else:
-            params = QueryDict(mutable=False)
-
-        if host.endswith('/'):
-            host = host[:-1]
-
-        try:
-            scheme = params.pop('backend', None)[0]
-        except IndexError:
-            raise InvalidCacheBackendError, "Backend not set"
-
-        try:
-            self.prefix = params.pop('prefix', [''])[0]
-        except IndexError:
-            self.prefix = ''
-
-        backend_uri = '%s://%s/?%s' % (scheme, host, params.urlencode())
-
-        self.cache = get_cache(backend_uri)
+        self.cache = get_cache(settings.CACHE_BACKEND_REAL)
 
         if hasattr(self.cache, 'close'):
             self.close = self.cache.close
 
-    def _realm(self, key, global_prefix, **kwargs):
+    def _realm(self, key, **kwargs):
         realm = kwargs.pop('realm', None)
         logger.debug('Getting realm: %s' % realm)
 
@@ -74,37 +42,34 @@ class CacheClass(BaseCache):
 
             key = ':'.join([prefix, key])
 
-        if global_prefix:
-            key = ':'.join([global_prefix, key])
-
         return (key, kwargs)
 
     def add(self, key, *args, **kwargs):
-        key, kwargs = self._realm(key, self.prefix, **kwargs)
+        key, kwargs = self._realm(key, **kwargs)
         logger.debug('Adding key: %s' % key)
         return self.cache.add(key, *args, **kwargs)
 
     def get(self, key, *args, **kwargs):
-        key, kwargs = self._realm(key, self.prefix, **kwargs)
+        key, kwargs = self._realm(key, **kwargs)
         logger.debug('Getting key: %s' % key)
         return self.cache.get(key, *args, **kwargs)
 
     def set(self, key, *args, **kwargs):
-        key, kwargs = self._realm(key, self.prefix, **kwargs)
+        key, kwargs = self._realm(key, **kwargs)
         logger.debug('Setting key: %s' % key)
         return self.cache.set(key, *args, **kwargs)
 
     def delete(self, key, *args, **kwargs):
-        key, kwargs = self._realm(key, self.prefix, **kwargs)
+        key, kwargs = self._realm(key, **kwargs)
         logger.debug('Deleting key: %s' % key)
         return self.cache.delete(key, *args, **kwargs)
 
     def get_many(self, keys, *args, **kwargs):
-        key, kwargs = self._realm(key, self.prefix, **kwargs)
+        key, kwargs = self._realm(key, **kwargs)
         logger.debug('Gettings keys: %s' % keys)
         return self.cache.get_many(keys, *args, **kwargs)
 
     def has_key(self, key, *args, **kwargs):
-        key, kwargs = self._realm(key, self.prefix, **kwargs)
+        key, kwargs = self._realm(key, **kwargs)
         logger.debug('Checking key: %s' % key)
         return self.cache.has_key(key, *args, **kwargs)
