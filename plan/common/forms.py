bb963a65c370ff2f5d53ca24ed090903d4116fbf
Thomas Adamcik <timeplan@adamcik.no>
2009-02-02 13:35:39 +0100

    Support norwegian time format

--- plan/common/forms.py
+++ plan/common/forms.py
@@ -43,6 +43,8 @@ class DeadlineForm(forms.models.ModelForm):
         self.fields['userset'].widget.attrs['style'] = 'width: 7em'
         self.fields['userset'].label_from_instance = lambda obj: obj.name or obj.course.name
 
+        self.fields['time'].input_formats = ['%H:%M', '%H.%M']
+
         self.fields['time'].widget.attrs['size'] = 2
         self.fields['date'].widget.attrs['size'] = 7
         self.fields['task'].widget.attrs['size'] = 28
