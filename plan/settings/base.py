c65d059774e7a2f04a6078b4d8da4fb4c593ead4
Thomas Adamcik <timeplan@adamcik.no>
2009-08-03 14:19:07 +0200

    Remove test coverage settings from base settings

--- plan/settings/base.py
+++ plan/settings/base.py
@@ -125,28 +125,6 @@ COMPRESS_JS = {
     },
 }
 
-#TEST_RUNNER = 'plan.common.test_runner.test_runner_with_coverage'
-
-COVERAGE_MODULES = (
-    'plan.common.admin',
-    'plan.common.cache',
-    'plan.common.forms',
-    'plan.common.logger',
-    'plan.common.managers',
-    'plan.common.middleware',
-    'plan.common.models',
-    'plan.common.timetable',
-    'plan.common.urls',
-    'plan.common.utils',
-    'plan.common.views',
-#    'plan.scrape.db',
-#    'plan.scrape.studweb',
-    'plan.ical.urls',
-    'plan.ical.views',
-    'plan.pdf.urls',
-    'plan.pdf.views',
-)
-
 TIMETABLE_MAX_COLORS = 8
 TIMETABLE_AJAX_LIMIT = 100
 TIMETABLE_COOKIE_AGE = 60*60*24*7*4
