fd45ddd8fd63a73cb2db705b47196be630a86eaf
Thomas Adamcik <timeplan@adamcik.no>
2009-08-03 14:19:33 +0200

    Add settings file for coverage

---
+++ plan/settings/coverage.py
@@ -0,0 +1,21 @@
+#TEST_RUNNER = 'plan.common.test_runner.test_runner_with_coverage'
+
+COVERAGE_MODULES = (
+    'plan.common.admin',
+    'plan.common.cache',
+    'plan.common.forms',
+    'plan.common.logger',
+    'plan.common.managers',
+    'plan.common.middleware',
+    'plan.common.models',
+    'plan.common.timetable',
+    'plan.common.urls',
+    'plan.common.utils',
+    'plan.common.views',
+#    'plan.scrape.db',
+#    'plan.scrape.studweb',
+    'plan.ical.urls',
+    'plan.ical.views',
+    'plan.pdf.urls',
+    'plan.pdf.views',
+)
