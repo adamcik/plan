735568a4ab8ff26a19fa8d19bc1d839d277364b6
Thomas Adamcik <timeplan@adamcik.no>
2009-08-04 13:21:12 +0000

    Add comment to settings

--- plan/settings/base.py
+++ plan/settings/base.py
@@ -139,4 +139,6 @@ CACHE_TIME_FRONTPAGE = 10*60        # 10m
 CACHE_TIME_HELP      = 2*60         #  2m
 CACHE_TIME_AJAX      = 60*60*24*7   #  1w
 
+# Hostname to be used in UID of ical events, should be a constant service-name
+# as UID changes will trigger email notices for new events in some cases.
 ICAL_HOSTNAME = socket.getfqdn()
