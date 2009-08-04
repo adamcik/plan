76ee30ec445067d67b59485af908baaaaf49fecb
Thomas Adamcik <timemplan@adamcik.no>
2009-08-04 13:22:08 +0000

    Update custom slugify method

--- plan/common/templatetags/slugify.py
+++ plan/common/templatetags/slugify.py
@@ -5,19 +5,20 @@ from django import template
 
 register = template.Library()
 
+REPLACE_MAP = (
+    (u'Æ', u'Ae'),
+    (u'Ø', u'O'),
+    (u'Å', u'Aa'),
+    (u'æ', u'ae'),
+    (u'ø', u'o'),
+    (u'å', u'aa'),
+)
+
+
 @register.filter
 @stringfilter
 def slugify(text):
-    replace_map = (
-        (u'Æ', u'Ae'),
-        (u'Ø', u'Oe'),
-        (u'Å', u'Aa'),
-        (u'æ', u'ae'),
-        (u'ø', u'oe'),
-        (u'å', u'aa'),
-    )
-
-    for old, new in replace_map:
+    for old, new in REPLACE_MAP:
         text = text.replace(old, new)
 
     return django_slugify(text)
