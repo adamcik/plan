22897f7f41e2690614407320f43b8722c385277c
Thomas Adamcik <timemplan@adamcik.no>
2009-08-04 13:21:50 +0000

    Remove unused templatetag

--- plan/common/templatetags/strip.py
+++ plan/common/templatetags/strip.py
@@ -19,16 +19,3 @@ class StripNode(template.Node):
     def render(self, context):
         output = self.nodelist.render(context)
         return re.sub(self.regexp, ' ', output).strip()
-
-@register.tag(name='stripblank')
-def do_stripblank(parser, token):
-    nodelist = parser.parse(('endstripblank',))
-    parser.delete_first_token()
-    return BlankNode(nodelist)
-
-class BlankNode(template.Node):
-    def __init__(self, nodelist):
-        self.nodelist = nodelist
-
-    def render(self, context):
-        return '\n'.join([line for line in self.nodelist.render(context).split('\n') if line.strip()])
