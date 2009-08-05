e79c28d37a2366c7efb4412dbb484130ed2aee54
Thomas Adamcik <timemplan@adamcik.no>
2009-08-05 11:59:17 +0000

    More migrations to help move semester-data from exams/lectures/sets to course

---
+++ plan/common/migrations/0004_remove_unique_constraint_on_course_names.py
@@ -0,0 +1,98 @@
+
+from south.db import db
+from django.db import models
+from plan.common.models import *
+
+class Migration:
+    
+    def forwards(self, orm):
+        db.delete_unique('common_course', ['name']) 
+    
+    def backwards(self, orm):
+        db.create_unique('common_course', ['name']) 
+    
+    models = {
+        'common.course': {
+            'full_name': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'points': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '5', 'decimal_places': '2'}),
+            'semester': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'foo'", 'null': 'True', 'to': "orm['common.Semester']"}),
+            'semesters': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['common.Semester']", 'null': 'True', 'blank': 'True'}),
+            'url': ('django.db.models.fields.URLField', [], {'max_length': '200', 'blank': 'True'}),
+            'version': ('django.db.models.fields.CharField', [], {'max_length': '20', 'null': 'True', 'blank': 'True'})
+        },
+        'common.deadline': {
+            'date': ('django.db.models.fields.DateField', [], {'default': 'datetime.date(2009, 8, 12)'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'task': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'time': ('django.db.models.fields.TimeField', [], {'null': 'True', 'blank': 'True'}),
+            'userset': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['common.UserSet']"})
+        },
+        'common.exam': {
+            'comment': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'course': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['common.Course']"}),
+            'duration': ('django.db.models.fields.PositiveSmallIntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'exam_date': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'exam_time': ('django.db.models.fields.TimeField', [], {'null': 'True', 'blank': 'True'}),
+            'handout_date': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'handout_time': ('django.db.models.fields.TimeField', [], {'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'semester': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['common.Semester']", 'null': 'True'}),
+            'type': ('django.db.models.fields.CharField', [], {'max_length': '1', 'blank': 'True'}),
+            'type_name': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'})
+        },
+        'common.group': {
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '100'})
+        },
+        'common.lecture': {
+            'course': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['common.Course']"}),
+            'day': ('django.db.models.fields.PositiveSmallIntegerField', [], {}),
+            'end': ('django.db.models.fields.TimeField', [], {}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['common.Group']", 'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'lecturers': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['common.Lecturer']", 'null': 'True', 'blank': 'True'}),
+            'rooms': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['common.Room']", 'null': 'True', 'blank': 'True'}),
+            'semester': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['common.Semester']"}),
+            'start': ('django.db.models.fields.TimeField', [], {}),
+            'type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['common.Type']", 'null': 'True', 'blank': 'True'}),
+            'weeks': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['common.Week']", 'null': 'True', 'blank': 'True'})
+        },
+        'common.lecturer': {
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '200'})
+        },
+        'common.room': {
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '100'})
+        },
+        'common.semester': {
+            'Meta': {'unique_together': "[('year', 'type')]"},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'type': ('django.db.models.fields.PositiveSmallIntegerField', [], {}),
+            'year': ('django.db.models.fields.PositiveSmallIntegerField', [], {})
+        },
+        'common.type': {
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '100'}),
+            'optional': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'blank': 'True'})
+        },
+        'common.userset': {
+            'Meta': {'unique_together': "(('slug', 'course', 'semester'),)"},
+            'added': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'course': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['common.Course']"}),
+            'exclude': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['common.Lecture']", 'null': 'True', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['common.Group']", 'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
+            'semester': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['common.Semester']"}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '50', 'db_index': 'True'})
+        },
+        'common.week': {
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'number': ('django.db.models.fields.PositiveIntegerField', [], {'unique': 'True'})
+        }
+    }
+    
+    complete_apps = ['common']
