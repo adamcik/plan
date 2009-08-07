bc705f03ad9c6127aab1005e8c74f4051ee70d50
Thomas Adamcik <timemplan@adamcik.no>
2009-08-07 13:56:12 +0000

    Add AGPLv3 License

--- plan/common/forms.py
+++ plan/common/forms.py
@@ -1,5 +1,22 @@
 # encoding: utf-8
 
+# Copyright 2008, 2009 Thomas Kongevold Adamcik
+
+# This file is part of Plan.
+#
+# Plan is free software: you can redistribute it and/or modify
+# it under the terms of the Affero GNU General Public License as 
+# published by the Free Software Foundation, either version 3 of
+# the License, or (at your option) any later version.
+#
+# Plan is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# Affero GNU General Public License for more details.
+#
+# You should have received a copy of the Affero GNU General Public
+# License along with Plan.  If not, see <http://www.gnu.org/licenses/>.
+
 from django import forms
 from django.db.models import Q
 
