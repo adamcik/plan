7ef658f70ef70528b9821dd0337b1741adbf3e0b
Thomas Adamcik <timemplan@adamcik.no>
2009-08-12 08:32:06 +0000

    Add Course.get_stats test case and fix bug in get stats

--- plan/common/tests/models.py
+++ plan/common/tests/models.py
@@ -17,9 +17,29 @@
 # License along with Plan.  If not, see <http://www.gnu.org/licenses/>.
 
 from plan.common.tests.base import BaseTestCase
+from plan.common.models import Course, Semester
 
 class ModelsTestCase(BaseTestCase):
-    pass
+    fixtures = ['test_data.json', 'test_user.json']
+
+    def test_course_get_stats(self):
+        semester = Semester.objects.get(year=2009, type=Semester.SPRING)
+        actual = Course.get_stats(semester)
+
+        self.assertEquals(3, actual.pop('slug_count'))
+        self.assertEquals(3, actual.pop('course_count'))
+        self.assertEquals(6, actual.pop('subscription_count'))
+        self.assertEquals(3, actual.pop('deadline_count'))
+
+        stats = actual.pop('stats')
+
+        self.assertEquals((3, 2, u'COURSE2', u'Course 2 full name'), stats[0])
+        self.assertEquals((2, 1, u'COURSE1', u'Course 1 full name'), stats[1])
+        self.assertEquals((1, 3, u'COURSE3', u'Course 3 full name'), stats[2])
+
+        self.assertEquals(15, actual.pop('limit'))
+
+        self.assertEquals({}, actual)
 
     # FIXME test unicode
     # FIXME test course.get_url
