d24935af14e64e03a47eddddf5660277d1c54398
Thomas Adamcik <timeplan@adamcik.no>
2009-11-27 16:25:32 +0100

    Add managment command for loading rooms

---
+++ plan/scrape/management/commands/rooms.py
@@ -0,0 +1,46 @@
+# Copyright 2008, 2009 Thomas Kongevold Adamcik
+
+# This file is part of Plan.
+#
+# Plan is free software: you can redistribute it and/or modify
+# it under the terms of the Affero GNU General Public License as
+# published by the Free Software Foundation, either version 3 of
+# the License, or (at your option) any later version.
+#
+# Plan is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# Affero GNU General Public License for more details.
+#
+# You should have received a copy of the Affero GNU General Public
+# License along with Plan.  If not, see <http://www.gnu.org/licenses/>.
+
+import logging
+
+from django.core.management.base import BaseCommand
+from django.db import transaction
+
+from plan.scrape.rooms import update_rooms
+from plan.common.logger import init_console
+
+init_console()
+
+logger = logging.getLogger()
+
+class Command(BaseCommand):
+
+    @transaction.commit_manually
+    def handle(self, *args, **options):
+        try:
+            update_rooms()
+
+            if raw_input('Save changes? [y/N] ').lower() == 'y':
+                transaction.commit()
+                print 'Saving changes...'
+            else:
+                transaction.rollback()
+                print 'Ignoring changes...'
+
+        except:
+            transaction.rollback()
+            raise
