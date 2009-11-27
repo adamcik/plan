20411d6f22d5cf00f1ff0087a9a84366871b5277
Thomas Adamcik <timeplan@adamcik.no>
2009-11-27 12:32:02 +0100

    Add migration to get rid of dynamic urls

---
+++ plan/common/migrations/0036_switch_to_non_dynamic_urls.py
@@ -0,0 +1,152 @@
+
+from south.db import db
+from django.db import models
+from plan.common.models import *
+
+class Migration:
+    no_dry_run = True
+
+    def forwards(self, orm):
+        for course in orm.Course.objects.exclude(url=''):
+            values = course.__dict__
+            for key in values.keys():
+                if type(values[key]) in [unicode, str]:
+                    values['%s_lower' % key] = values.get(key, '').lower()
+                    values['%s_upper' % key] = values.get(key, '').upper()
+                else:
+                    values['%s_lower' % key] = values[key]
+                    values['%s_upper' % key] = values[key]
+
+            course.url = course.url % values
+            course.save()
+
+    def backwards(self, orm):
+        pass
+
+    models = {
+        'auth.group': {
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '80', 'unique': 'True'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'unique_together': "(('content_type', 'codename'),)"},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True', 'blank': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'blank': 'True'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'blank': 'True'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'max_length': '30', 'unique': 'True'})
+        },
+        'common.course': {
+            'Meta': {'unique_together': "[('code', 'semester', 'version')]"},
+            'code': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'points': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '5', 'decimal_places': '2', 'blank': 'True'}),
+            'semester': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['common.Semester']"}),
+            'url': ('django.db.models.fields.URLField', [], {'max_length': '200', 'blank': 'True'}),
+            'version': ('django.db.models.fields.CharField', [], {'max_length': '20', 'null': 'True', 'blank': 'True'})
+        },
+        'common.deadline': {
+            'date': ('django.db.models.fields.DateField', [], {}),
+            'done': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'subscription': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['common.Subscription']"}),
+            'task': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'time': ('django.db.models.fields.TimeField', [], {'null': 'True', 'blank': 'True'})
+        },
+        'common.exam': {
+            'course': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['common.Course']"}),
+            'duration': ('django.db.models.fields.PositiveSmallIntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'exam_date': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'exam_time': ('django.db.models.fields.TimeField', [], {'null': 'True', 'blank': 'True'}),
+            'handout_date': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'handout_time': ('django.db.models.fields.TimeField', [], {'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['common.ExamType']", 'null': 'True', 'blank': 'True'})
+        },
+        'common.examtype': {
+            'code': ('django.db.models.fields.CharField', [], {'max_length': '20', 'unique': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'})
+        },
+        'common.group': {
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100', 'unique': 'True'})
+        },
+        'common.lecture': {
+            'course': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['common.Course']"}),
+            'day': ('django.db.models.fields.PositiveSmallIntegerField', [], {}),
+            'end': ('django.db.models.fields.TimeField', [], {}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['common.Group']", 'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'lecturers': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['common.Lecturer']", 'null': 'True', 'blank': 'True'}),
+            'rooms': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['common.Room']", 'null': 'True', 'blank': 'True'}),
+            'start': ('django.db.models.fields.TimeField', [], {}),
+            'type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['common.LectureType']", 'null': 'True', 'blank': 'True'})
+        },
+        'common.lecturer': {
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '200', 'unique': 'True'})
+        },
+        'common.lecturetype': {
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100', 'unique': 'True'}),
+            'optional': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'blank': 'True'})
+        },
+        'common.room': {
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100', 'unique': 'True'})
+        },
+        'common.semester': {
+            'Meta': {'unique_together': "[('year', 'type')]"},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'type': ('django.db.models.fields.CharField', [], {'max_length': '10'}),
+            'year': ('django.db.models.fields.PositiveSmallIntegerField', [], {})
+        },
+        'common.student': {
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '50', 'unique': 'True', 'db_index': 'True'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']", 'null': 'True', 'blank': 'True'})
+        },
+        'common.subscription': {
+            'Meta': {'unique_together': "(('student', 'course'),)"},
+            'added': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'alias': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
+            'color': ('django.db.models.fields.CharField', [], {'max_length': '6', 'blank': 'True'}),
+            'course': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['common.Course']"}),
+            'exclude': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['common.Lecture']", 'null': 'True', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['common.Group']", 'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'student': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['common.Student']"})
+        },
+        'common.week': {
+            'Meta': {'unique_together': "[('lecture', 'number')]"},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'lecture': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['common.Lecture']"}),
+            'number': ('django.db.models.fields.PositiveIntegerField', [], {})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'unique_together': "(('app_label', 'model'),)", 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        }
+    }
+
+    complete_apps = ['common']
