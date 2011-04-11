3fc99cd504f31e2e449b8b5c77b784d84f330c7d
Thomas Adamcik <adamcik@samfundet.no>
2011-04-11 23:05:31 +0200

    Add staticfiles context processors

--- plan/settings/base.py
+++ plan/settings/base.py
@@ -118,6 +118,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.i18n',
     'django.core.context_processors.media',
     'django.core.context_processors.request',
+    'staticfiles.context_processors.static',
     'plan.common.context_processors.source_url',
 )
 
