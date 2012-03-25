9e7013e9958aeeacb73ff160de612e605ddd4e05
Thomas Adamcik <thomas@adamcik.no>
2012-03-26 00:37:29 +0200

    Clear out unused context processors.

--- plan/settings/base.py
+++ plan/settings/base.py
@@ -124,11 +124,7 @@ TEMPLATE_DIRS = (
 
 TEMPLATE_CONTEXT_PROCESSORS = (
     'django.contrib.auth.context_processors.auth',
-    'django.core.context_processors.debug',
-    'django.core.context_processors.i18n',
-    'django.core.context_processors.media',
     'django.core.context_processors.request',
-    'staticfiles.context_processors.static',
     'plan.common.context_processors.source_url',
 )
 
