63de92ac28527784aea7530d79952be6b10058f4
Thomas Adamcik <thomas@adamcik.no>
2012-03-26 22:25:55 +0200

    Switch to cached template loading.

--- plan/settings/base.py
+++ plan/settings/base.py
@@ -96,8 +96,10 @@ INSTALLED_APPS = (
 # -- Template settings:
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
+    ('django.template.loaders.cached.Loader', (
+        'django.template.loaders.filesystem.Loader',
+        'django.template.loaders.app_directories.Loader',
+    )),
 )
 
 TEMPLATE_DIRS = (
