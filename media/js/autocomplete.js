4aac92b99d95ebf6eaabe6de4a3ad81a7b8ea289
Thomas Adamcik <thomas@adamcik.no>
2012-07-16 00:25:26 +0200

    Pass autocomplete url via data attribute.

--- media/js/autocomplete.js
+++ media/js/autocomplete.js
@@ -1,14 +1,13 @@
-/* This file is part of the plan timetable generator, see LICENSE for details. yy*/
+/* This file is part of the plan timetable generator, see LICENSE for details. */
 
 $(function() {
-  try { autocomplete_url } catch (e) { autocomplete_url = "" }
-
-  $('#course').autocomplete(autocomplete_url, {
+  var course = $('#course');
+  course.autocomplete(course.attr('data-autocomplete'), {
     max: 100,
     minChars: 3,
     multiple: true,
     formatItem: function(row) { return row[0] + ': ' + row[1]},
     formatResult: function(row) { return row[0] },
-    width: $('#course').outerWidth()
+    width: course.outerWidth()
   });
 });
