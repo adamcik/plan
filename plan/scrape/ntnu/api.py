051898085eaa420674e5f29bca507048d86e898f
Thomas Adamcik <thomas@adamcik.no>
2012-08-05 21:04:46 +0200

    Pull out reusable bits from NTNU api code.

--- plan/scrape/ntnu/api.py
+++ plan/scrape/ntnu/api.py
@@ -5,22 +5,41 @@
 import logging
 import json
 
-from plan.common.models import Semester
+from plan.common.models import Course, Semester
 from plan.scrape import utils
 from plan.scrape import base
 
+TERM_MAPPING = {
+    Semester.SPRING: 'Spring',
+    Semester.FALL: 'Autumn',
+}
+
+
+def fetch_course(code):
+    url = 'http://www.ime.ntnu.no/api/course/%s' % (
+        code.lower().encode('utf-8'))
+
+    try:
+        logging.debug('Retrieving %s', url)
+        return json.loads(utils.cached_urlopen(url))['course']
+    except IOError as e:
+        logging.error('Loading falied: %s', e)
+        return None
+
+
+def match_term(semester, course):
+    year = semester.year
+    term = TERM_MAPPING[semester.type]
+
+    for t in course.get('educationTerm', []):
+        if t['year'] == year and t['termApplies'] == term:
+            return True
+    return False
+
 
 class Courses(base.CourseScraper):
-    TERM_MAPPING = {
-        Semester.SPRING: 'Spring',
-        Semester.FALL: 'Autumn',
-    }
-
     def fetch(self):
         data = utils.cached_urlopen('http://www.ime.ntnu.no/api/course/-')
-
-        current_year = self.semester.year
-        current_term = self.TERM_MAPPING[self.semester.type]
 
         for c in json.loads(data)['course']:
             raw_code = '%s-%s' % (c['code'], c['versionCode'])
@@ -30,30 +49,15 @@ class Courses(base.CourseScraper):
                 logging.warning('Skipped invalid course name: %s', raw_code)
                 continue
 
-            url = 'http://www.ime.ntnu.no/api/course/%s' % (
-                code.lower().encode('utf-8'))
-
-            try:
-                logging.debug('Retrieving %s', url)
-                course = json.loads(utils.cached_urlopen(url))['course']
-            except IOError as e:
-                logging.error('Loading falied: %s', e)
+            course = fetch_course(code)
+            if not course:
                 continue
 
-            for term in course.get('educationTerm', []):
-                right_year = term['year'] == current_year
-                right_term = term['termApplies'] == current_term
-
-                if not (right_year and right_term):
-                    continue
-
+            if match_term(self.semester, course):
                 yield {'code': code,
                        'name': course['name'],
                        'version': version,
                        'points': course['credit'],
                        'url': 'http://www.ntnu.no/studier/emner/%s' % code}
-                break
             else:
-                # If we make it here the course has no educationTerms or none
-                # that match this semester, so indicate that we need remove it.
                 yield {'delete': True, 'code': code, 'version': version}
