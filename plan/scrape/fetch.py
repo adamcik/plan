fd9fa489fb36479da69719fd9fb15f8fd2bc8b2a
Thomas Adamcik <thomas@adamcik.no>
2012-08-09 20:19:32 +0200

    Log if we are getting URL from cache or internet.

--- plan/scrape/fetch.py
+++ plan/scrape/fetch.py
@@ -11,13 +11,17 @@ from django.core import cache
 scraper_cache = cache.get_cache('scraper')
 
 
-def get(url, cache=True):
+def get(url, cache=True, verbose=False):
     data = scraper_cache.get(url)
+    msg = 'Cached fetch: %s' % url
     if not data or not cache:
+        msg = 'Fetched: %s' % url
         response = urllib.urlopen(url)
         data = response.read()
         if response.getcode() == 200 and data:
             scraper_cache.set(url, data)
+
+    logging.log(logging.INFO if verbose else logging.DEBUG, msg)
     return data
 
 
@@ -25,13 +29,8 @@ def plain(url, query=None, verbose=False, cache=True):
     if query:
         url += '?' + urllib.urlencode(query)
 
-    if verbose:
-        logging.info('Retrieving: %s' % url)
-    else:
-        logging.debug('Retrieving: %s' % url)
-
     try:
-       return get(url, cache=cache)
+       return get(url, cache=cache, verbose=verbose)
     except IOError as e:
         logging.error('Loading %s falied: %s', url, e)
 
