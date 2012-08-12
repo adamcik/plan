b1222967f2d3217f2e36ea7168484d0f79963180
Thomas Adamcik <thomas@adamcik.no>
2012-08-12 18:35:39 +0200

    Extract web lecture row parsing.

--- plan/scrape/ntnu/web.py
+++ plan/scrape/ntnu/web.py
@@ -16,20 +16,6 @@ from plan.scrape import utils
 LETTERS = u'ABCDEFGHIJKLMNOPQRSTUVWXYZÆØÅ'
 
 
-def fetch_rooms():
-    result = fetch.html('http://www.ntnu.no/studieinformasjon/rom/')
-    if result is None:
-        return
-
-    rooms = {}
-    for option in result.cssselect('.hovedramme select[name="romnr"] option'):
-        code = utils.clean_string(option.attrib['value'])
-        name = utils.clean_string(option.text_content())
-
-        if code and name and 'ikkerom' not in name:
-            yield code, name
-
-
 # TODO(adamcik): consider using http://www.ntnu.no/web/studier/emner
 #   ?p_p_id=courselistportlet_WAR_courselistportlet_INSTANCE_emne
 #   &_courselistportlet_WAR_courselistportlet_INSTANCE_emne_year=2011
@@ -107,42 +93,70 @@ class Lectures(base.LectureScraper):
 
             lecture_type = None
             for tr in table.cssselect('tr')[1:-1]:
-                data = {}
-
-                for i, td in enumerate(tr.cssselect('td')):
-                    if i == 0:
-                        if td.attrib.get('colspan', 1) == '4':
-                            lecture_type = td.text_content().strip()
-                        else:
-                            time = td.cssselect('b')[0].text_content().strip()
-                            raw_day, period = time.split(' ', 1)
-                            raw_start, raw_end = period.split('-')
-
-                            data['day'] = utils.parse_day_of_week(raw_day)
-                            data['start'] = utils.parse_time(raw_start)
-                            data['end'] = utils.parse_time(raw_end)
-
-                            match = re.match('.*Uke: (.+)', td.text_content())
-                            data['weeks'] = utils.parse_weeks(match.group(1))
-                    elif i == 1 and len(td.cssselect('a')) == 1:
-                        a = td.cssselect('a')[0]
-                        rooms = [a.text] + [e.tail for e in a]
-
-                        data['rooms'] = []
-                        for name in utils.clean_list(rooms, utils.clean_string):
-                            if name not in room_codes:
-                                data['rooms'].append((None, name))
-                                continue
-
-                            if len(room_codes[name]) > 1:
-                                logging.warning('Multiple rooms with name %s, '
-                                                'simply using first code.', name)
-                            data['rooms'].append((room_codes[name][0], name))
-                    elif i == 2:
-                        data['lecturers'] = [td.text] + [e.tail for e in td]
-                    elif i == 3:
-                        data['groups'] = [g.text_content() for g in td.cssselect('span')]
-
-                if data:
+                data = parse_row(tr, room_codes)
+                if data.get('lecture_type', None):
+                    lecture_type = data['lecture_type']
+                elif data:
                     data.update({'course': course, 'type': lecture_type})
                     yield data
+
+
+def fetch_rooms():
+    result = fetch.html('http://www.ntnu.no/studieinformasjon/rom/')
+    if result is None:
+        return
+
+    rooms = {}
+    for option in result.cssselect('.hovedramme select[name="romnr"] option'):
+        code = utils.clean_string(option.attrib['value'])
+        name = utils.clean_string(option.text_content())
+
+        if code and name and 'ikkerom' not in name:
+            yield code, name
+
+
+def parse_row(tr, room_codes):
+    data = {}
+    for i, td in enumerate(tr.cssselect('td')):
+        if i == 0:
+            if td.attrib.get('colspan', 1) == '4':
+                lecture_type = utils.clean_string(td.text_content())
+                if lecture_type:
+                    data['lecture_type'] = lecture_type
+            else:
+                time = td.cssselect('b')[0].text_content().strip()
+                raw_day, period = time.split(' ', 1)
+                raw_start, raw_end = period.split('-')
+
+                data['day'] = utils.parse_day_of_week(raw_day)
+                data['start'] = utils.parse_time(raw_start)
+                data['end'] = utils.parse_time(raw_end)
+
+                match = re.match('.*Uke: (.+)', td.text_content())
+                data['weeks'] = utils.parse_weeks(match.group(1))
+        elif i == 1 and len(td.cssselect('a')) > 0:
+            if len(td.cssselect('a')) > 1:
+                logging.warning('Multiple rooms links found, simply '
+                                'using first one.')
+
+            a = td.cssselect('a')[0]
+            rooms = [a.text] + [e.tail for e in a]
+
+            data['rooms'] = []
+            for name in utils.clean_list(rooms, utils.clean_string):
+                if name not in room_codes:
+                    data['rooms'].append((None, name))
+                    continue
+
+                if len(room_codes[name]) > 1:
+                    logging.warning('Multiple rooms with name %s, '
+                                    'simply using first code.', name)
+                data['rooms'].append((room_codes[name][0], name))
+        elif i == 2:
+            data['lecturers'] = [td.text] + [e.tail for e in td]
+        elif i == 3:
+            data['groups'] = [g.text_content() for g in td.cssselect('span')]
+
+    return data
+
+
