fbb737538e5ac2761f0991b7471730ae03666ecb
Thomas Adamcik <thomas@adamcik.no>
2015-09-13 17:51:57 +0200

    Bump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = "plan",
-    version = "1.5",
+    version = "1.6",
     url = 'https://github.com/adamcik/plan',
     license = 'AGLv3',
     description = 'Timetable generator for educational institutions.',
