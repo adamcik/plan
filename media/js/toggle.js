bb3145a0d9a3143fa9a0478cf87f6ed6629ec8c1
Thomas Adamcik <thomas@adamcik.no>
2016-09-05 14:06:02 +0200

    Fix init for toggle code

--- media/js/toggle.js
+++ media/js/toggle.js
@@ -1,29 +1,39 @@
 /* This file is part of the plan timetable generator, see LICENSE for details. */
 
-document.addEventListener('DOMContentLoaded', function() {
-  var groups = document.querySelectorAll('#change-groups .groupbox'),
-      template = document.getElementById('toogle-template'),
-      wrapper, inputs;
+(function() {
+  function init() {
+    document.removeEventListener('DOMContentLoaded', arguments.callee, false);
+    var groups = document.querySelectorAll('#change-groups .groupbox'),
+        template = document.getElementById('toogle-template'),
+        wrapper, inputs;
 
-  var toggle = function(state) {
-    for (var j=0; j < this.length; j++) {
-      if (state) {
-        this[j].setAttribute('checked', 'checked');
-      } else {
-        this[j].removeAttribute('checked');
+    var toggle = function(state) {
+      for (var j=0; j < this.length; j++) {
+        if (state) {
+          this[j].setAttribute('checked', 'checked');
+        } else {
+          this[j].removeAttribute('checked');
+        }
       }
-    }
-  };
+    };
 
-  for (var i=0; i < groups.length; i++) {
-    inputs = groups[i].querySelectorAll('input');
-    wrapper = template.cloneNode(true);
-    wrapper.removeAttribute('id');
-    wrapper.removeAttribute('style');
-    wrapper.querySelector('.toogle-all').addEventListener(
-      'click', toggle.bind(inputs, true));
-    wrapper.querySelector('.toogle-none').addEventListener(
-      'click', toggle.bind(inputs, false));
-    groups[i].appendChild(wrapper);
+    for (var i=0; i < groups.length; i++) {
+      inputs = groups[i].querySelectorAll('input');
+      wrapper = template.cloneNode(true);
+      wrapper.removeAttribute('id');
+      wrapper.removeAttribute('style');
+      wrapper.querySelector('.toogle-all').addEventListener(
+        'click', toggle.bind(inputs, true));
+      wrapper.querySelector('.toogle-none').addEventListener(
+        'click', toggle.bind(inputs, false));
+      groups[i].appendChild(wrapper);
+    }
   }
-}, false);
+
+  if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', init, false);
+  } else {
+    init();
+  }
+})();
+
