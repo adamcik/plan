a250e95a37af9786733e23e8f836918a6bf69af9
Thomas Adamcik <thomas@adamcik.no>
2021-12-31 00:34:19 +0100

    Add akademika scraper

---
+++ plan/scrape/ntnu/akademika.py
@@ -0,0 +1,23 @@
+# This file is part of the plan timetable generator, see LICENSE for details.
+
+from plan.scrape import base
+from plan.scrape import fetch
+
+
+def fetch_syllabus(code):
+    query = {'curriculum': code.encode('utf-8')}
+    root = fetch.html('https://www.akademika.no/curriculum/search', query=query)
+    for e in root.cssselect('.curriculum-search-result[data-id]'):
+        if not e.xpath('.//a[contains(text(),"NTNU")]'):
+            continue
+        url = 'https://www.akademika.no/ajax/curriculum/' + e.attrib['data-id']
+        for (_, _, href, _) in fetch.html(url, verbose=True).iterlinks():
+             if href.startswith('/pensum/%s-' % code.lower()):
+                 return 'https://www.akademika.no' + href
+    return ''
+
+
+class Syllabus(base.SyllabusScraper):
+    def scrape(self):
+        for course in self.queryset():
+            yield {'code': course.code, 'syllabus': fetch_syllabus(course.code)}
