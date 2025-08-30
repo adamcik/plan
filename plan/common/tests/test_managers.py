020eb73eb3047c2a4195269b349f98645b32e40a
Thomas Adamcik <thomas@adamcik.no>
2025-08-30 12:10:08 +0200

    tests: Fix get_lectures tests

--- plan/common/tests/test_managers.py
+++ plan/common/tests/test_managers.py
@@ -1,6 +1,6 @@
 # This file is part of the plan timetable generator, see LICENSE for details.
 
-from plan.common.models import Course, Exam, Lecture, Semester, Subscription
+from plan.common.models import Course, Exam, Lecture, Semester, Student, Subscription
 from plan.common.tests import BaseTestCase
 
 
@@ -11,25 +11,24 @@ class ManagerTestCase(BaseTestCase):
         # Exclude lectures connected to other courses and excluded from userset
         control = Lecture.objects.exclude(id__in=[6, 7])
 
-        # Try showing all lectures
-        lectures = Lecture.objects.get_lectures(2009, Semester.SPRING, "adamcik")
-        lectures = [l for l in lectures if l.show_week and not l.exclude]
-        self.assertEqual(set(lectures), set(control))
+        semester = Semester.objects.get(year=2009, type=Semester.SPRING)
+        student = Student.objects.get(slug="adamcik")
+        lectures = Lecture.objects.get_lectures(semester.id, student.id)
+
+        actual = [l for l in lectures if not l.exclude]
+        self.assertEqual(set(actual), set(control))
 
         # Try showing only lectures in week 1
-        lectures = Lecture.objects.get_lectures(2009, Semester.SPRING, "adamcik", 1)
-        lectures = [l for l in lectures if l.show_week and not l.exclude]
-        self.assertEqual(set(lectures), set(control.filter(weeks__number=1)))
+        actual = [l for l in lectures if 1 in l.week_numbers and not l.exclude]
+        self.assertEqual(set(actual), set(control.filter(weeks__number=1)))
 
         # Try showing lectures in week 2
-        lectures = Lecture.objects.get_lectures(2009, Semester.SPRING, "adamcik", 2)
-        lectures = [l for l in lectures if l.show_week and not l.exclude]
-        self.assertEqual(set(lectures), set(control.filter(weeks__number=2)))
+        actual = [l for l in lectures if 2 in l.week_numbers and not l.exclude]
+        self.assertEqual(set(actual), set(control.filter(weeks__number=2)))
 
         # Try lectures in week 3, ie none
-        lectures = Lecture.objects.get_lectures(2009, Semester.SPRING, "adamcik", 3)
-        lectures = [l for l in lectures if l.show_week and not l.exclude]
-        self.assertEqual(set(lectures), set())
+        actual = [l for l in lectures if 3 in l.week_numbers and not l.exclude]
+        self.assertEqual(set(actual), set())
 
     def test_get_exams(self):
         exams = Exam.objects.get_exams(2009, Semester.SPRING, "adamcik")
