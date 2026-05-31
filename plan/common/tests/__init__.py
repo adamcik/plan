809edbd9fd5c6fda3cb38042ee3d9f73561a4383
Thomas Adamcik <thomas@adamcik.no>
2026-05-31 14:45:28 +0200

    test: add strict template variable helper for migration

--- plan/common/tests/__init__.py
+++ plan/common/tests/__init__.py
@@ -1,8 +1,11 @@
 # This file is part of the plan timetable generator, see LICENSE for details.
 
 import datetime
+import copy
 
+from django.conf import settings
 from django.test import TestCase
+from django.test import override_settings
 from django.urls import reverse
 
 from plan.common.models import Semester, Student
@@ -41,3 +44,24 @@ class BaseTestCase(TestCase):
 
     def url_basic(self, name):
         return reverse(name)
+
+
+class StrictTemplateVariable:
+    def __contains__(self, item):
+        return item == "%s"
+
+    def __mod__(self, missing):
+        raise RuntimeError(f"Missing template variable or attribute: {missing}")
+
+    def __str__(self):
+        raise RuntimeError("Missing template variable or attribute")
+
+
+def strict_template_variables():
+    templates = copy.deepcopy(settings.TEMPLATES)
+    for backend in templates:
+        if backend.get("BACKEND") != "django.template.backends.django.DjangoTemplates":
+            continue
+        options = backend.setdefault("OPTIONS", {})
+        options["string_if_invalid"] = StrictTemplateVariable()
+    return override_settings(TEMPLATES=templates)
